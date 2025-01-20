c7b920a7e3ba0bb4c5c7759b7a7111a5c1abab97
bohdan.snopok <snopokbogdan3@gmail.com>
2025-01-20 20:13:09 +0200

    improved auth logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import './App.css';
-import {Header} from "./components/Header/Header.jsx"
+import {Header} from "./components/Header/Header.jsx";
 import {BlogPage} from "./containers/BlogPage/BlogPage";
 import {Footer} from "./components/Footer/Footer";
 import {LoginPage} from "./containers/LoginPage/LoginPage";
 import {useState} from "react";
 
 export function App() {
-
     const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
-    const [userName, setUserName] = useState(localStorage.getItem('userName'))
+    const [userName, setUserName] = useState(localStorage.getItem('userName'));
 
     return (
         <Router>
@@ -18,10 +17,28 @@ export function App() {
 
                 <main>
                     <Routes>
-                        <Route path="/" element={<BlogPage/>}/>
+                        <Route
+                            path="/"
+                            element={
+                                isLoggedIn ? <Navigate to="/blog"/> : <Navigate to="/login"/>
+                            }
+                        />
                         <Route
                             path="/login"
-                            element={<LoginPage setIsLoggedIn={setIsLoggedIn} setUserName={setUserName}/>}
+                            element={
+                                !isLoggedIn ?
+                                    <LoginPage
+                                        setIsLoggedIn={setIsLoggedIn}
+                                        setUserName={setUserName}
+                                    /> : <Navigate to="/blog"/>
+                            }
+                        />
+                        <Route
+                            path="/blog"
+                            element={
+                                isLoggedIn ?
+                                    <BlogPage/> : <Navigate to="/login"/>
+                            }
                         />
                     </Routes>
                 </main>
